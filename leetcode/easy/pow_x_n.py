50a101a263ad9648e97c6bd39fdacb6268291e9d
rashad101 <rah.rony@gmail.com>
2019-07-28 10:40:05 +0200

    file added

---
+++ leetcode/easy/pow_x_n.py
@@ -0,0 +1,4 @@
+#https://leetcode.com/problems/powx-n/
+
+#Runtime: 8 ms, faster than 99.40% of Python online submissions for Pow(x, n).
+#Memory Usage: 11.9 MB, less than 23.40% of Python online submissions for Pow(x, n).
