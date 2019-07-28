81dc0b4c07f87a738f680998e6b0c875161b1d9f
rashad101 <rah.rony@gmail.com>
2019-07-28 10:14:26 +0200

    file added

--- leetcode/easy/add_binary.py
+++ leetcode/easy/add_binary.py
@@ -1,3 +1,7 @@
+#https://leetcode.com/problems/add-binary/
+
+#Runtime: 20 ms, faster than 81.64% of Python online submissions for Add Binary.
+#Memory Usage: 11.8 MB, less than 31.13% of Python online submissions for Add Binary.
 class Solution(object):
     def addBinary(self, a, b):
         """
