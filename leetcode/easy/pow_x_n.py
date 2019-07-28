7a38e84bf9de9b46abec37a43836e5612e634442
rashad101 <rah.rony@gmail.com>
2019-07-28 10:43:39 +0200

    file added

--- leetcode/easy/pow_x_n.py
+++ leetcode/easy/pow_x_n.py
@@ -2,3 +2,13 @@
 
 #Runtime: 8 ms, faster than 99.40% of Python online submissions for Pow(x, n).
 #Memory Usage: 11.9 MB, less than 23.40% of Python online submissions for Pow(x, n).
+
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        import math
+        return float("{0:.5f}".format(math.pow(x,n)))
