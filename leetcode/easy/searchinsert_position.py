8b30254dd66af82c306ce0935b46d34175059f0b
rashad101 <rah.rony@gmail.com>
2019-07-30 22:53:01 +0200

    file added

--- leetcode/easy/searchinsert_position.py
+++ leetcode/easy/searchinsert_position.py
@@ -1,1 +1,12 @@
 #https://leetcode.com/problems/search-insert-position/
+
+
+#Runtime: 36 ms, faster than 68.80% of Python online submissions for Search Insert Position.
+#Memory Usage: 12.1 MB, less than 96.79% of Python online submissions for Search Insert Position.
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
