e27b65d2a737d3cdd02b6f522fda9fc63f1e83c0
Md Rashad Al Hasan Rony <rah.rony@gmail.com>
2019-08-02 12:28:02 +0200

    Update single_number.py

--- leetcode/easy/single_number.py
+++ leetcode/easy/single_number.py
@@ -1,1 +1,19 @@
 
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        mapping = dict()
+        
+        for a in nums:
+            if a in mapping:
+                mapping[a]+=1
+            else:
+                mapping[a] = 1
+                
+        for k,_ in mapping.items():
+            if mapping[k]==1:
+                return k
+        
