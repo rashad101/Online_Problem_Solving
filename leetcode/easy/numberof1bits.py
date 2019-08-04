85e9ca4bef78a3b945574068c9a23667b27bbda4
rashad101 <rah.rony@gmail.com>
2019-08-04 10:46:21 +0200

    number of 1 bit

--- leetcode/easy/numberof1bits.py
+++ leetcode/easy/numberof1bits.py
@@ -1,6 +1,7 @@
 #https://leetcode.com/problems/number-of-1-bits/submissions/
 
-
+#Runtime: 16 ms, faster than 80.88% of Python online submissions for Number of 1 Bits.
+#Memory Usage: 11.8 MB, less than 18.61% of Python online submissions for Number of 1 Bits.
 class Solution(object):
     def hammingWeight(self, n):
         """
