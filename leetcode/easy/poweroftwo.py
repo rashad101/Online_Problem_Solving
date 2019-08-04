775933d9160f63ab8586dd82e852536c07edbcbe
rashad101 <rah.rony@gmail.com>
2019-08-04 12:56:12 +0200

    power of 2

--- leetcode/easy/poweroftwo.py
+++ leetcode/easy/poweroftwo.py
@@ -1,1 +1,22 @@
 #https://leetcode.com/problems/power-of-two/
+
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+
+        if n == 1 or n == 2:
+            return True
+
+        if n % 2 != 0 or n == 0:
+            return False
+
+        while (n != 1):
+            if n % 2 == 0:
+                n = n // 2
+            else:
+                return False
+
+        return True
