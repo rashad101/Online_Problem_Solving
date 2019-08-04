a2fceee4e08e29cfcf25e2ed89be4ddcbb560d2c
rashad101 <rah.rony@gmail.com>
2019-08-04 10:45:48 +0200

    number of 1 bit

--- leetcode/easy/numberof1bits.py
+++ leetcode/easy/numberof1bits.py
@@ -1,1 +1,16 @@
 #https://leetcode.com/problems/number-of-1-bits/submissions/
+
+
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        n = bin(n)[2:]
+        binstring = str(n)
+        count = 0
+        for i in range(len(binstring)):
+            if binstring[i]=="1":
+                count+=1
+        return count
